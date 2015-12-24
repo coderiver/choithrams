94beeece68ea7ad69825cf404e30816c0ba1edc8
hraboviyvadim <hraboviyvadim@gmail.com>
2015-12-24 21:36:24 +0700

    Product page

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,59 @@
 $(document).ready(function() {
 
-	// go js
+	// main menu dropdown
+
+	$(document).on('click', function(){
+		$('.js-menu-btn').removeClass('is-open');
+		$('.js-menu').slideUp('fast');
+	});
+
+	$('.js-menu').on('click', function(event){
+		event.stopPropagation();
+	});
+
+	$('.js-menu-btn').on('click', function(event){
+		if ($(this).hasClass('is-open')) {
+			$(this).removeClass('is-open');
+			$('.js-menu').slideUp('fast');
+		}
+		else {
+			$(this).addClass('is-open');
+			$('.js-menu').slideDown('fast');
+		}
+		event.stopPropagation();
+	});
+
+	// product images gallery
+	$('.js-product-view').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		fade: true,
+		asNavFor: '.js-product-nav'
+	});
+	$('.js-product-nav').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		asNavFor: '.js-product-view',
+		focusOnSelect: true,
+		arrows: false
+	});
+
+	// js-substitution
+
+	$('.js-substitution').slick({
+		infinite: false,
+		slidesToShow: 3,
+		slidesToScroll: 1
+	});	
+
+	// item-slider
+
+	$('.js-item-slider').slick({
+		slidesToScroll: 1,
+		slidesToShow: 5,
+		arrows: true
+	});
+
 
 });
